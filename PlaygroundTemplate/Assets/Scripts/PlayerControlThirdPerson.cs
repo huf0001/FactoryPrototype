d925b99502d4588fd9819979299b169d11cb8105
DESKTOP-5S357S1\sarah <101265321@student.swin.edu.au>
2018-08-10 18:09:14 +1000

    Adding character with stationary camera; camera rotates with player tho;

--- PlaygroundTemplate/Assets/Scripts/PlayerControlThirdPerson.cs
+++ PlaygroundTemplate/Assets/Scripts/PlayerControlThirdPerson.cs
@@ -1,21 +1,21 @@
 using UnityEngine;
 using System.Collections;
 
-public class PhysControllerScript : MonoBehaviour
+public class PlayerControlThirdPerson : MonoBehaviour
 
 {
     public Rigidbody player;
-    public Transform playerCam, playerTransform; //centerPoint;
+    public Transform playerCam, playerTransform, centerPoint;
 
     public float moveSpeed = 2f;
     private Vector3 movement;
     private Vector3 lookInputs;
     private Vector3 movePos;
 
-    //public float zoom;
-    //private float zoomSpeed = 3;
-    //private float zoomMin = -4f;
-    //private float zoomMax = -10f; 
+    public float zoom;
+    private float zoomSpeed = 3;
+    private float zoomMin = -4f;
+    private float zoomMax = -10f; 
 
     private float mouseX, mouseY, moveFrontBack, moveLeftRight;
 
@@ -24,13 +24,13 @@ public class PhysControllerScript : MonoBehaviour
     {
         Cursor.lockState = CursorLockMode.Locked;
         Cursor.visible = false;
-        //zoom = -7f;
+        zoom = -7f;
     }
     
     // Update is called once per frame
     void Update ()
     {
-        /*allows player to rotater camera up and down with right click
+        //allows player to rotater camera up and down with right click
         if (Input.GetMouseButton (1)) 
         {
             mouseX -= Input.GetAxis ("Mouse Y");
@@ -39,28 +39,10 @@ public class PhysControllerScript : MonoBehaviour
         //controls camera rotation constraints when using right click
         mouseX = Mathf.Clamp(mouseX, -39f, 60f);
         playerCam.LookAt(centerPoint);
-        centerPoint.localRotation = Quaternion.Euler (mouseX, 0, 0);*/
-		
-        //gathering movement input 
-        //moveFrontBack = Input.GetAxis ("Vertical"); 
-        //moveLeftRight = Input.GetAxis ("Horizontal");
-
-        //controls movement settings/dir
-        //movement = new Vector3 (moveLeftRight, 0, moveFrontBack);
-        //Vector3 newDir = playerCam.transform.TransformDirection (movement);
-        //movePos = new Vector3 (newDir.x * moveSpeed, 0, newDir.z * moveSpeed);
-
-        //gathering look input
-        lookInputs.y += Input.GetAxis("Mouse X");
-        lookInputs.x -= Input.GetAxis("Mouse Y");
-        lookInputs.x = Mathf.Clamp (lookInputs.x, -20f, 20f);
+        centerPoint.localRotation = Quaternion.Euler (mouseX, 0, 0);
     }
 
     void FixedUpdate ()
     {
-        //updates player movement & rotation
-        Vector3 newRot = new Vector3 (lookInputs.x, lookInputs.y, 0);
-        Quaternion deltaRotation = Quaternion.Euler (newRot);
-        player.rotation = deltaRotation;
     }
 }
