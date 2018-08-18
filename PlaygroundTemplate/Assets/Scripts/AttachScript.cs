23f37e7af4303de00f49695e6b94e96d3409f868
huf0001 <samhuffer01@gmail.com>
2018-08-18 16:57:03 +1000

    Rudimentry attachability functionality is now working. See Cube (17) and AttachScript.cs for details. Still wanna add some code so that each guide object will be added to the list of available guides automatically when the game is run, without needing to be assigned in the inspector beforehand, so that we can alter the number of guides for each attachable object and not have to mess around with assigning them in the inspector or changing the code to allow that number of guides.

---
+++ PlaygroundTemplate/Assets/Scripts/AttachScript.cs
@@ -0,0 +1,94 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AttachScript : MonoBehaviour
+{
+    private GameObject item;
+    private GameObject item2;
+    private GameObject item3;
+    private GameObject item4;
+    public Transform guide1;
+    public Transform guide2;
+    public Transform guide3;
+    public Transform guide4;
+
+    Dictionary<Transform, GameObject> AttachedItems;
+    List<Transform> AvailableGuides;
+
+    public string attachableTag;
+
+    private void Start()
+    {
+        AttachedItems = new Dictionary<Transform, GameObject>();
+        AvailableGuides = new List<Transform>();
+
+        ListGuide(guide1);
+        ListGuide(guide2);
+        ListGuide(guide3);
+        ListGuide(guide4);
+    }
+
+    private void ListGuide(Transform guide)
+    {
+        if (guide != null)
+        {
+            AvailableGuides.Add(guide);
+        }
+    }
+
+    //If an attachable object collides with this object, attach it
+    private void OnTriggerStay(Collider other)
+    {
+        if ((other.tag == attachableTag) && (CheckCanAttach()))
+        {
+            Attach(other.gameObject);
+        }
+    }
+
+    private bool CheckCanAttach()
+    {
+        bool result = true;
+
+        if (AvailableGuides.Count == 0)
+        {
+            result = false;
+        }
+
+        return result;
+    }
+
+    //Attaches attachable objects to this object
+    private void Attach(GameObject attaching)
+    {
+        AssignAttaching(attaching);
+        attaching.GetComponent<Rigidbody>().useGravity = false;
+        attaching.GetComponent<Rigidbody>().isKinematic = true;
+        attaching.transform.parent = this.gameObject.transform;
+        attaching.tag = "Attached";
+    }
+
+    //Assigns the object being attached to an appropriate attached object slot (and guide object?)
+    private void AssignAttaching(GameObject attaching)
+    {
+        Transform tempGuide = AvailableGuides[0];
+        AvailableGuides.Remove(tempGuide);
+        AttachedItems.Add(tempGuide, attaching);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        foreach (KeyValuePair<Transform, GameObject> p in AttachedItems)
+        {
+            UpdateAttached(p.Value, p.Key);
+        }
+    }
+
+    //Updates position and location for the specified attached object
+    private void UpdateAttached(GameObject attachedItem, Transform attachedGuide)
+    {
+        attachedItem.transform.position = attachedGuide.transform.position;
+        attachedItem.transform.rotation = attachedGuide.transform.rotation;
+    }
+}
--- PlaygroundTemplate/Assets/Scripts/MoveObjectScript.cs
+++ PlaygroundTemplate/Assets/Scripts/MoveObjectScript.cs
@@ -7,11 +7,13 @@ public class MoveObjectScript : MonoBehaviour
     public GameObject item;
     public GameObject tempParent;
     public Transform guide;
+    private string storedTag;
 
 	// Use this for initialization
 	void Start ()
     {
         item.GetComponent<Rigidbody>().useGravity = true;
+        storedTag = "";
 	}
 
     void OnMouseDown()
@@ -21,6 +23,7 @@ public class MoveObjectScript : MonoBehaviour
         item.transform.position = guide.transform.position;
         item.transform.rotation = guide.transform.rotation;
         item.transform.parent = tempParent.transform;
+        storedTag = this.tag;
         this.tag = "PlayerMoving";
     }
 
@@ -30,6 +33,15 @@ public class MoveObjectScript : MonoBehaviour
         item.GetComponent<Rigidbody>().isKinematic = false;
         item.transform.parent = null;
         item.transform.position = guide.transform.position;
-        this.tag = "Untagged";
+        this.tag = "DroppedByPlayer";
+    }
+
+    private void OnCollisionTrigger(Collision collision)
+    {
+        if (collision.gameObject.tag != "AttachBase")
+        {
+            this.tag = storedTag;
+            storedTag = "";
+        }
     }
 }
