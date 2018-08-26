8ba062b31ce82d4bbc347f17be95909bc7ed776d
DESKTOP-5S357S1\sarah <101265321@student.swin.edu.au>
2018-08-26 14:05:28 +1000

    both hands now move following the players head; need to get arms to go higher

--- PlaygroundTemplate/Assets/Scripts/PlayerControlThirdPerson.cs
+++ PlaygroundTemplate/Assets/Scripts/PlayerControlThirdPerson.cs
@@ -6,8 +6,8 @@ public class PlayerControlThirdPerson : MonoBehaviour
 {
     public Rigidbody player;
     public Transform playerCam, playerTransform;
-    public Rigidbody rightHand;
-    public float grabSpeed = 2f;
+    public Rigidbody rightHand, leftHand;
+    public float grabSpeed = 5f;
     public Transform playerHead;
 
     public float moveSpeed = 2f;
@@ -15,6 +15,7 @@ public class PlayerControlThirdPerson : MonoBehaviour
     private Vector3 lookInputs;
     private Vector3 movePos;
     private bool grabR = false;
+    private bool grabL = false;
 
     private float mouseX, mouseY, moveFrontBack, moveLeftRight;
 
@@ -47,6 +48,9 @@ public class PlayerControlThirdPerson : MonoBehaviour
         {
             grabR = true;
         }
+        if (Input.GetMouseButton(0)) {
+            grabL = true;
+        }
     }
 
     void FixedUpdate ()
@@ -55,10 +59,10 @@ public class PlayerControlThirdPerson : MonoBehaviour
         player.velocity = movePos;
         Vector3 camRotation = new Vector3(lookInputs.x, lookInputs.y, 0);
         Quaternion cDeltaRotation = Quaternion.Euler(camRotation);
-        //Vector3 newRot = new Vector3 (0, lookInputs.y, 0);
-        //Quaternion deltaRotation = Quaternion.Euler (newRot);
-        //player.rotation = deltaRotation; commented out because conflicts with camera rotation when active 
-        //playerCam.rotation = cDeltaRotation; commented out for testing
+        Vector3 newRot = new Vector3 (0, lookInputs.y, 0);
+        Quaternion deltaRotation = Quaternion.Euler (newRot);
+        player.rotation = deltaRotation;  
+        //playerCam.rotation = cDeltaRotation;
         playerHead.rotation = cDeltaRotation;
 
 
@@ -67,9 +71,17 @@ public class PlayerControlThirdPerson : MonoBehaviour
         //moves hands based on players head rotation 
         //right hand
         if (grabR) {
-            rightHand.AddForce(playerHead.forward * grabSpeed);
-            rightHand.velocity = Vector3.zero;
+            rightHand.AddForce(playerHead.forward * grabSpeed, ForceMode.Acceleration);
             grabR = false;
+            rightHand.velocity = Vector3.zero; 
+        }
+
+        //left hand
+        if (grabL)
+        {
+            leftHand.AddForce(playerHead.forward * grabSpeed, ForceMode.Acceleration);
+            grabL = false;
+            leftHand.velocity = Vector3.zero;
         }
     }
 }
