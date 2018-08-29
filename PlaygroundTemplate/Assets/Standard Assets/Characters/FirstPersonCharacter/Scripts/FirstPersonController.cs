68f7d1a8ca9eaae4917826ea664a939bfec22b4d
huf0001 <samhuffer01@gmail.com>
2018-08-29 20:34:21 +1000

    Just saving my tinkering in the Playground Scene

--- PlaygroundTemplate/Assets/Standard Assets/Characters/FirstPersonCharacter/Scripts/FirstPersonController.cs
+++ PlaygroundTemplate/Assets/Standard Assets/Characters/FirstPersonCharacter/Scripts/FirstPersonController.cs
@@ -17,19 +17,19 @@ namespace UnityStandardAssets.Characters.FirstPerson
         //[SerializeField] private GameObject playerAvatar;
 
         [SerializeField] private bool m_IsWalking;
-        [SerializeField] private float m_WalkSpeed;
-        [SerializeField] private float m_RunSpeed;
-        [SerializeField] [Range(0f, 1f)] private float m_RunstepLenghten;
-        [SerializeField] private float m_JumpSpeed;
-        [SerializeField] private float m_StickToGroundForce;
-        [SerializeField] private float m_GravityMultiplier;
+        [SerializeField] private float m_WalkSpeed = 5;
+        [SerializeField] private float m_RunSpeed = 10;
+        [SerializeField] [Range(0f, 1f)] private float m_RunstepLenghten = 0.7f;
+        [SerializeField] private float m_JumpSpeed = 10;
+        [SerializeField] private float m_StickToGroundForce = 10;
+        [SerializeField] private float m_GravityMultiplier = 2;
         [SerializeField] private MouseLook m_MouseLook;
-        [SerializeField] private bool m_UseFovKick;
+        [SerializeField] private bool m_UseFovKick = true;
         [SerializeField] private FOVKick m_FovKick = new FOVKick();
-        [SerializeField] private bool m_UseHeadBob;
+        [SerializeField] private bool m_UseHeadBob = false;
         [SerializeField] private CurveControlledBob m_HeadBob = new CurveControlledBob();
         [SerializeField] private LerpControlledBob m_JumpBob = new LerpControlledBob();
-        [SerializeField] private float m_StepInterval;
+        [SerializeField] private float m_StepInterval = 5;
         [SerializeField] private AudioClip[] m_FootstepSounds;    // an array of footstep sounds that will be randomly selected from.
         [SerializeField] private AudioClip m_JumpSound;           // the sound played when character leaves the ground.
         [SerializeField] private AudioClip m_LandSound;           // the sound played when character touches back on ground.
