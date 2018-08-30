eff1162ec561c7a528543d3db5f93bd29232a4b6
huf0001 <samhuffer01@gmail.com>
2018-08-30 21:12:02 +1000

    Conveyer belt bin now kills whatever falls in. Should probably make it exclude the player. Don't jump in the bin, it'll kill you. Let shit list people do that instead.

---
+++ PlaygroundTemplate/Assets/Scripts/BinScript.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BinScript : MonoBehaviour
+{
+
+    // Should be physically impossible for anything to enter the bin except via falling in, so it can't be triggered from the sides.
+    // Will check that the item in question is not the player though.
+    private void OnTriggerStay(Collider other)
+    {
+        if (true)
+        {
+            Destroy(other.gameObject);
+        }
+    }
+}
