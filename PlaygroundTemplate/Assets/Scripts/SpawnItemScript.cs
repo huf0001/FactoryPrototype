b89a2b1498db983569688b736f146383f0f63eb4
huf0001 <samhuffer01@gmail.com>
2018-09-02 19:50:39 +1000

    Conveyor belt spawning is done. Make a conveyor belt, assign prefabs of objects it can create, assign how many times each will spawn compared to the others, hit play, and watch it pump out those objects. Will be destroyed when they collide with a trigger collider in the bin. Don't jump in the bin though, it doesn't work on you, you still stay there to suffer the disapointment that is reality.

---
+++ PlaygroundTemplate/Assets/Scripts/SpawnItemScript.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+public class SpawnItemScript : MonoBehaviour
+{
+    [System.Serializable]
+    public class ItemWeightPair
+    {
+        [SerializeField] private GameObject item;
+        [SerializeField] private int itemWeighting;
+
+        public GameObject Item
+        {
+            get
+            {
+                return item;
+            }
+        }
+
+        public int ItemWeighting
+        {
+            get
+            {
+                return itemWeighting;
+            }
+        }
+    }
+
+    [SerializeField] private float secBetweenSpawns = 2f;
+    [SerializeField] ItemWeightPair[] itemWeightPairs;
+
+    private List<GameObject> weightedObjects = new List<GameObject>();
+    private float time = 0f;
+    
+    // Use this for initialization
+	void Start ()
+    {
+        foreach (ItemWeightPair p in itemWeightPairs)
+        {
+            for (int i = 0; i < p.ItemWeighting; i++)
+            {
+                weightedObjects.Add(p.Item);
+            }
+        }
+
+        Debug.Log("weightedObjects.Count is :" + weightedObjects.Count);
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (time > secBetweenSpawns)
+        {
+            Spawn();
+            time = Time.deltaTime;
+        }
+        else
+        {
+            time += Time.deltaTime;
+        }
+	}
+
+    private void Spawn()
+    {
+        int i = Random.Range(0, weightedObjects.Count);
+        GameObject spawning = Instantiate(weightedObjects[i]);
+        spawning.transform.position = this.gameObject.transform.position;
+        spawning.transform.rotation = this.gameObject.transform.rotation;
+    }
+}
